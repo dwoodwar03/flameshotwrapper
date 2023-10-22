b2f33c79345efdc90dcb0007f5d67925c83538bb
Dave Woodward <dave@woodwardmail.com>
2023-10-22 10:16:14 +0100

    Forwards cmdline arguments to flameshot app
    Likely buffer overrun if called with parameters > approx 1000 chars though.

--- src/flameshotfix.c
+++ src/flameshotfix.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(void)
-{
-    system("flameshot");
+int main(int argc, char *argv[]) {
+
+    int i;
+    char cmdline[1000];
+
+    strcpy(cmdline, "flameshot");
+
+    for (i=1; i < argc; i++) {
+        strcat(cmdline, " ");
+        strcat(cmdline, argv[i]);
+    }
+
+    system(cmdline);
 }
